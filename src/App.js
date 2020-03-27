d447fc967fdad3113001f7a846137de432608bf1
Ubuntu <ubuntu@ip-172-31-87-88.ec2.internal>
2020-03-27 18:32:30 +0000

    No Changes reclean Jeremy

--- src/App.js
+++ src/App.js
@@ -6,14 +6,7 @@ import Header from './components/Header'
 import CatIndex from './pages/CatIndex'
 import CatShow from './pages/CatShow'
 import NewCat from './pages/NewCat'
-<<<<<<< HEAD
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      cats: [], // We start with an empty array, so the component can finish rendering before we make our fetch request
-=======
 class App extends Component {
   constructor(props){
     super(props)
@@ -22,56 +15,13 @@ class App extends Component {
     }
     this.getCats() // Calls our fetch method when the component loads for the first time
     }
-    handleNewCat = cat => {
-      console.log({cat})
-    }
     componentWillMount(){
     	this.getCats()
     }
-
-    getCats = () => {
-       // Making a fetch request to the url of our Rails app
-       // fetch returns a promise
-      fetch("http://localhost:3000/cats")
-      .then((response)=>{
-        //Make sure we get a successful response back
-        if(response.status === 200){
-          // We need to convert the response to JSON
-          // This also returns a promise
-          return(response.json())  
-        }
-      })
-      .then((catsArray)=>{
-         //Finally, we can assign the cats to state, and they will render
-        this.setState({ cats: catsArray })
-      })
-    }
-    render(){
-      return(
-        <React.Fragment>
-          <Header/>
-          <Router>
-            <Switch>
-              <Route exact path="/cat/:id" component={ CatShow } />
-              <Route exact path="/createcat" render={ (props) => <NewCat handleNewCat={ this.handleNewCat } /> }/>
-              <Route exact path="/" render={ (props) => <CatIndex cats={ this.state.cats } /> } />
-            </Switch>
-          </Router>
-        </React.Fragment>
-      )
->>>>>>> 928d51dd1c4cf09d3c0b32a88b95bf5ab388b008
-    }
-    this.getCats() // Calls our fetch method when the component loads for the first time
-  }
-
-  componentDidMount() {
-    this.getCats()
-  }
-
   getCats = () => {
     // Making a fetch request to the url of our Rails app
     // fetch returns a promise
-    fetch("http://localhost:3000/cats")
+    fetch("http://localhost:4000/cats")
       .then((response) => {
         //Make sure we get a successful response back
         if (response.status === 200) {
@@ -87,10 +37,9 @@ class App extends Component {
         })
       })
   }
-<<<<<<< HEAD
 
   handleCreateCat = (newcat) => {
-    return fetch("http://127.0.0.1:3000/cats", {
+    return fetch("http://0.0.0.0:4000/cats", {
       // converting an object to a string
     	body: JSON.stringify(newcat),
       // specify the info being sent in JSON and the info returning should be JSON
@@ -129,11 +78,6 @@ class App extends Component {
       )
     }
   }
-=======
-  
- 
-
->>>>>>> 928d51dd1c4cf09d3c0b32a88b95bf5ab388b008
 
 
 
